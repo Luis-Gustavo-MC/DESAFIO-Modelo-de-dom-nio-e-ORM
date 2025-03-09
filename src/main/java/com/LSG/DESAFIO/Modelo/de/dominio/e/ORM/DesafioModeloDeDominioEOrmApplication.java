0c1a5c82cdb7fcac4cc56e5b12875c129f2697da
Luis Gustavo <lgmc120@gmail.com>
2025-03-09 18:27:20 -0300

    create spring project

---
+++ src/main/java/com/LSG/DESAFIO/Modelo/de/dominio/e/ORM/DesafioModeloDeDominioEOrmApplication.java
@@ -0,0 +1,13 @@
+package com.LSG.DESAFIO.Modelo.de.dominio.e.ORM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioModeloDeDominioEOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioModeloDeDominioEOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LSG/DESAFIO/Modelo/de/dominio/e/ORM/DesafioModeloDeDominioEOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LSG.DESAFIO.Modelo.de.dominio.e.ORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioModeloDeDominioEOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
